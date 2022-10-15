57337bbe91312b5cacfab6cef5b1ee198478e4ec
Abdurasulov Maksat <maxabdurasulov@gmail.com>
2022-10-15 19:27:31 +0600

    homestart done

--- src/Pages/Home/sections/Start/HomeStart.tsx
+++ src/Pages/Home/sections/Start/HomeStart.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './homeStart.scss';
 import MoneyBag from './img/moneyBag.png';
-// import ActiveTasks from './img/activeTasks.png';
-// import Orders from './img/orders.png';
+import ActiveTasks from './img/activeTasks.png';
+import Orders from './img/orders.png';
+import DoTasks from './img/doTasks.png';
+import Money from './img/money.png';
+import Friends from './img/friends.png';
+import Cup from './img/cup.png';
+import StartBg from './img/homeStartBg.png';
 
 const HomeStart = () => {
     return (
@@ -16,8 +21,64 @@ const HomeStart = () => {
                             <p>Выплачено денег</p>
                         </div>
                     </div>
+                    <div className="homeStart__cards_item">
+                        <div className="homeStart__cards_item-content">
+                            <img src={ActiveTasks} alt=""/>
+                            <h5>24 250</h5>
+                            <p>Заказчиков</p>
+                        </div>
+                    </div>
+                    <div className="homeStart__cards_item">
+                        <div className="homeStart__cards_item-content">
+                            <img src={Orders} alt=""/>
+                            <h5>3 526</h5>
+                            <p>Активных заданий</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="homeStart__info">
+                    <h2 className="homeStart__info_title">С чего начать</h2>
+                    <div className="homeStart__info_row">
+                        <div className="homeStart__info_item">
+                            <div className="homeStart__info_item-img">
+                                <img src={DoTasks} alt=""/>
+                            </div>
+                            <div className="homeStart__info_item-texts">
+                                <h5>Выполняйте задания</h5>
+                                <p>Выполняйте интересные задания от заказчиков в свободное для Вас время</p>
+                            </div>
+                        </div>
+                        <div className="homeStart__info_item">
+                            <div className="homeStart__info_item-img">
+                                <img src={Money} alt=""/>
+                            </div>
+                            <div className="homeStart__info_item-texts">
+                                <h5>Зарабатывайте деньги</h5>
+                                <p>Получайте честно заработанные деньги сразу же после проверки задания</p>
+                            </div>
+                        </div>
+                        <div className="homeStart__info_item">
+                            <div className="homeStart__info_item-img">
+                                <img src={Friends} alt=""/>
+                            </div>
+                            <div className="homeStart__info_item-texts">
+                                <h5>Привлекайте друзей</h5>
+                                <p>Зарабатывайте до 40% с каждого выполнения Вашего реферала</p>
+                            </div>
+                        </div>
+                        <div className="homeStart__info_item">
+                            <div className="homeStart__info_item-img">
+                                <img src={Cup} alt=""/>
+                            </div>
+                            <div className="homeStart__info_item-texts">
+                                <h5>Получите результат</h5>
+                                <p>Тысячи надежных исполнителей готовы выполнить любое Ваше задание</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <img className="homeStart__bg" src={StartBg} alt=""/>
         </section>
     );
 };
