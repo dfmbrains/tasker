787151cfe0b5d14c8cd7c0b3f92166a58bbf193a
Nurekov <killerbuny3@gmail.com>
2022-10-22 14:29:58 +0600

    comments component ready

--- src/Components/Reviews/Reviews.tsx
+++ src/Components/Reviews/Reviews.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import './Reviews.scss'
+import CommentCard from "../commentCard/CommentCard";
+import ShowMoreBtn from "../ShowMoreButton/ShowMoreBtn";
 
 interface Interface {
     username: string
 }
 
 const Reviews: React.FC<Interface> = ({username}) => {
+    const arr = [1, 2, 3];
     return (
         <div className="reviews">
             <h2 className="reviews__about">Отзывы о {username}</h2>
             <div className="reviews__messageRow">
-                <div className="reviews__messageRow_message">
-                    <div className="avatar">E</div>
-                    <span className="mission">Разработка логотипа по эскизу, картинке. Логотип в векторе по эскизу</span>
-                </div>
+                {arr.map((item) => (
+                    <CommentCard/>
+                ))}
             </div>
+            <div style={{display: 'flex', justifyContent: 'center', paddingTop: '40px'}}>
+                <ShowMoreBtn/>
+            </div>
+
         </div>
     );
 };
