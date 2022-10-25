fa8bbfcca49df1acbc0622eda7523d6432c4365c
Nurekov <killerbuny3@gmail.com>
2022-10-25 23:56:58 +0600

    adsga

--- src/Pages/VacancyOffers/index.tsx
+++ src/Pages/VacancyOffers/index.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import MarketPlaceAside from "../../Components/MarketPlaceAside/MarketPlaceAside";
 import {radioData1} from "../../Utils/radioData";
-import MyTasksCard from "../../Components/MyTasksCard";
 import './vacancyOffers.scss';
-import PortFolioCard from "../../Components/PortFolioCard/PortFolioCard";
-import CommentCard from "../../Components/commentCard/CommentCard";
-import ServiceCard from "../../Components/ServiceCard/ServiceCard";
 import VacancyCard from "../../Components/VacancyCard/VacancyCard";
 import TaskCard from "../../Components/TaskCard/TaskCard";
 
